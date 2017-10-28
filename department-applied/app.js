7ba118db46f40e5d2729ae43055c844e7fc8aa23
shubham jangid <shubhamjangid70@gmail.com>
2017-10-28 18:08:31 +0530

    Add files via upload

---
+++ department-applied/app.js
@@ -0,0 +1,18 @@
+
+$('.menu-toggle').click(function() {
+
+  $('.side-nav').toggleClass('side-nav--open',200);
+  $('.menu-toggle').toggleClass('menu-toggle--open');
+
+});
+
+
+$('.content').hide();
+$('.lab').on('click', function(){
+  if(!($(this).children('.content').is(':visible'))){
+    $('.content').slideUp();
+    $(this).children('.content').slideDown();
+  } else {
+    $('.content').slideUp();
+  }
+});
